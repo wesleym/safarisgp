943a79dcc7fbde696f02f36a547123ecea3d171c
Wesley Moy <wesley.moy@gmail.com>
2012-09-28 12:24:21 -0700

    Clean up SafariSGP code
    * Remove logging statements
    * Move listener into separately defined function

--- SafariSGP.safariextension/safarisgp.js
+++ SafariSGP.safariextension/safarisgp.js
@@ -1,7 +1,9 @@
-safari.application.addEventListener("popover", function() {
-  var url = safari.application.activeBrowserWindow.activeTab.url;
-  console.log(url);
-  var uriResult = URI.parse(url);
-  console.log(uriResult.getDomain());
-  document.getElementById("Domain").value = uriResult.getDomain();
-}, true);
+(function () {
+  function popoverListener() {
+    var url = safari.application.activeBrowserWindow.activeTab.url;
+    var uriResult = URI.parse(url);
+    document.getElementById("Domain").value = uriResult.getDomain();
+  }
+
+  safari.application.addEventListener("popover", popoverListener, true);
+}())
